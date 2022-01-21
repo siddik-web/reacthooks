1fbb5d5f43f160a543542006d883a38fd03254e6
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-01-21 23:23:28 +0600

    Update count value and Object value using useState hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [obj, setObj] = useState({'name': ''})
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>useState Example</h2>
+        <p>You Clicked the button {count} times.</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        <h2>{obj.name}</h2>
+        <input type="text" value={obj.name} onChange={(e) => setObj({...obj, 'name': e.target.value})} />
       </header>
     </div>
   );
