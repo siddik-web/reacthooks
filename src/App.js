a45469a0878345cf546adc3740eb9ffab4f6e6f7
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-12-04 19:20:53 +0600

    add counter

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const handelClick = (e) => {
-    e.preventDefault();
-    const task = document.querySelector("#task");
-    if (task.value !== "") {
-      todos.push({ text: task.value});
-      setTodos([...todos], todos);
-    }
-    task.value = "";
-  };
+  const [count, setCount] = useState(() => {
+    console.log("run");
+    return 4;
+  });
 
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
   return (
-    <div className="container">
-      <header className="App-header">
-        <h2>Todo Example Using useState Hook</h2>
-      </header>
-      <div className="row">
-        <div className="col-auto">
-          <input type="text" id="task" className="form-control" />
-        </div>
-        <div className="col-auto">
-          <button onClick={handelClick} className="btn btn-primary">
-            Add
-          </button>
-        </div>
-       
-      </div>
-      <ul className="list-unstyled">
-            {todos.map((todo, index) => {
-              return (
-                <li key={index}>
-                  <input type="checkbox" className="checked"/>
-                  {todo.text}
-                </li>
-              );
-            })}
-          </ul>
-    </div>
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+    </>
   );
 }
 
